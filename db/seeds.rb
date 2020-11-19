4f4e6059fc065b7f2622c2e024ab1c8e4b97bbae
Rowan Douglas <rowan.douglas29@gmail.com>
2020-11-19 18:13:17 -0300

    cocktail CRUD complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Ingredient.destroy_all
+
+ingredients_url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_list = JSON.parse(open(ingredients_url).read)
+puts "Adding ingredients to DB..."
+ingredients_list["drinks"].each do |ingredient|
+  new_ingredient = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "#{new_ingredient.name} added :tropical_drink:"
+end
+
+
+
