bcdb1a5b33836c231519c7536a76a342e0e29587
Rowan Douglas <rowan.douglas29@gmail.com>
2020-11-20 16:37:44 -0300

    more styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,9 @@ ingredients_list["drinks"].each do |ingredient|
   puts "#{new_ingredient.name} added :tropical_drink:"
 end
 
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+cocktail = Cocktail.new(name: 'TEST COCKTAIL WITH IMAGE')
+cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
 
 
