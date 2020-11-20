a62b9bdd8efd22ad0f08c005cc4f6de5c5e438c0
Rowan Douglas <rowan.douglas29@gmail.com>
2020-11-19 23:05:25 -0300

    core functions complete

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,43 @@
+class DosesController < ApplicationController
+
+  before_action :find_cocktail, only: [ :new, :create ]
+  before_action :list_ingredients, only: [ :new, :create ]
+  before_action :dose_params, only: [ :create ]
+  
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail, notice: 'Dose was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktail_path(@dose.cocktail)
+    
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def list_ingredients
+    @ingredients = Ingredient.all
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
